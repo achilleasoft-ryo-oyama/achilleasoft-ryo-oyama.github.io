7c6eaee7b6972c7d041008e4729035ebc9ffef9a
ryo oyama <ryo.oyama@achilleasoft.com>
2022-07-31 01:49:46 +0900

    change page title & change font size

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Spacer, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
 const Top = () => {
 	return (
@@ -16,11 +16,7 @@ const BaseInfo = () => {
 	const myPhoto = require("../images/my_photo.jpeg");
 	return (
 		<Box>
-			<Flex
-				h={{ base: "200px", md: "250px", lg: "300px" }}
-				justifyContent="center"
-				align="center"
-			>
+			<Flex marginTop={5} h={"auto"} justifyContent="center" align={"center"}>
 				<Box>
 					<Image
 						boxSize={{ base: "150px", md: "180px", lg: "200px" }}
@@ -34,10 +30,10 @@ const BaseInfo = () => {
 					<Text fontSize={{ base: "xl", md: "2xl", lg: "3xl" }}>
 						大山　亮（Ryo Oyama）
 					</Text>
-					<Text fontSize={{ base: "lg", md: "xl", lg: "2xl" }}>
+					<Text fontSize={{ base: "md", md: "xl", lg: "2xl" }}>
 						所属1. アキレアソフト代表
 					</Text>
-					<Text fontSize={{ base: "lg", md: "xl", lg: "2xl" }}>
+					<Text fontSize={{ base: "md", md: "xl", lg: "2xl" }}>
 						所属2. 中小企業開発部課長
 					</Text>
 				</Box>
